6edf1df63d3a99d0c11b4a18ed9e8d6be8c05d89
spring <lingxiadeyidu@163.com>
2017-05-13 15:57:47 +0800

    添加测试数据脚本

--- eforum-front/src/main/java/org/eforum/front/controller/UserController.java
+++ eforum-front/src/main/java/org/eforum/front/controller/UserController.java
@@ -7,14 +7,12 @@ import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.eforum.entity.User;
+import org.eforum.exception.ServiceException;
 import org.eforum.param.UserParam;
 import org.eforum.produces.ResultJson;
 import org.eforum.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -25,14 +23,34 @@ public class UserController extends BaseController {
 
 	@ApiOperation(value = "用户接口", notes = "新增用户", code = 200, produces = "application/json")
 	@RequestMapping(value = "/user", method = RequestMethod.POST)
-	public Object addUser(@RequestBody UserParam userParam) throws IllegalAccessException, InvocationTargetException {
+	public Object addUser(@RequestBody UserParam userParam) {
 		User user = new User();
-		BeanUtils.copyProperties(user, userParam);
-		user.setPassword(DigestUtils.md5Hex(userParam.getPassword()));
-		userService.addUser(user);
+		try {
+			BeanUtils.copyProperties(user, userParam);
+			user.setPassword(DigestUtils.md5Hex(userParam.getPassword()));
+			userService.addUser(user);
+		} catch (Exception e) {
+			throw new ServiceException("保存用户信息出错", e);
+		}
 		return new ResultJson(true, "保存用户信息成功");
 	}
-	
+
+	@ApiOperation(value = "用户接口", notes = "获取用户文章列表", code = 200, produces = "application/json")
+	@RequestMapping(value = "/user/{userId}/article", method = RequestMethod.POST)
+	public Object listArticle(@PathVariable("userId") Long userId,
+							  Integer pageNumber,
+							  Integer pageSize) {
+		return null;
+	}
+
+	@ApiOperation(value = "用户接口", notes = "获取用户评论列表", code = 200, produces = "application/json")
+	@RequestMapping(value = "/user/{userId}/comment", method = RequestMethod.POST)
+	public Object listComment(@PathVariable("userId") Long userId,
+							  Integer pageNumber,
+							  Integer pageSize) {
+		return null;
+	}
+
 	@ApiOperation(value = "用户接口", notes = "修改密码", code = 200, produces = "application/json")
 	@RequestMapping(value = "/user/changePassword", method = RequestMethod.POST)
 	public Object changePassword(String newPassword) {
--- script/eforum.sql
+++ script/eforum.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` int(21) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `name` varchar(20) DEFAULT NULL COMMENT '用户名',
   `email` varchar(200) DEFAULT NULL COMMENT '电子邮箱',
   `password` varchar(50) DEFAULT NULL COMMENT '密码',
@@ -8,6 +8,10 @@ CREATE TABLE `user` (
   UNIQUE KEY `index_name` (`name`) USING HASH,
   UNIQUE KEY `index_email` (`email`) USING HASH
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `eforum`.`user` (`id`, `email`, `name`, `password`) VALUES ('1', 'test1@eforum.org', 'test1', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `eforum`.`user` (`id`, `email`, `name`, `password`) VALUES ('2', 'test2@eforum.org', 'test2', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `eforum`.`user` (`id`, `email`, `name`, `password`) VALUES ('3', 'test3@eforum.org', 'test3', 'e10adc3949ba59abbe56e057f20f883e');
 
 
 
@@ -40,3 +44,25 @@ INSERT INTO `article` (`id`, `content`, `create_time`, `title`, `description`) V
 INSERT INTO `article` (`id`, `content`, `create_time`, `title`, `description`) VALUES ('3', 'MyBatis 是支持普通 SQL查询，存储过程和高级映射的优秀持久层框架。', '2017-03-08 23:14:13', 'mybatis', 'mybatis介绍');
 INSERT INTO `article` (`id`, `content`, `create_time`, `title`, `description`) VALUES ('4', 'Spring Boot是由Pivotal团队提供的全新框架，其设计目的是用来简化新Spring应用的初始搭建以及开发过程。', '2017-03-08 23:14:13', 'springboot', 'springboot介绍');
 INSERT INTO `article` (`id`, `content`, `create_time`, `title`, `description`) VALUES ('5', 'Struts2是一个基于MVC设计模式的Web应用框架，它本质上相当于一个servlet，在MVC设计模式中，Struts2作为控制器(Controller)来建立模型与视图的数据交互。', '2017-03-08 23:14:13', 'struts2', 'struts2介绍');
+
+
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` varchar(255) DEFAULT NULL COMMENT '评论内容',
+  `create_time` date DEFAULT NULL COMMENT '创建时间',
+  `article_id` bigint(20) DEFAULT NULL COMMENT '文章主键',
+  `main_comment_id` bigint(20) DEFAULT NULL COMMENT '所属主评论主键',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户主键',
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('1', '评论xxx', '2017-05-09', '1', NULL, '1');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('2', '评论xxx', '2017-05-09', '1', NULL, '2');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('3', '评论xxx', '2017-05-09', '1', NULL, '1');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('4', '评论xxx', '2017-05-09', '1', NULL, '1');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('5', '评论xxx', '2017-05-09', '1', NULL, '1');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('6', '评论xxx', '2017-05-09', '1', NULL, '1');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('7', '评论xxx', '2017-05-09', '1', NULL, '1');
+INSERT INTO `eforum`.`comment` (`id`, `content`, `create_time`, `article_id`, `main_comment_id`, `user_id`) VALUES ('8', '评论xxx', '2017-05-09', '1', NULL, '1');
